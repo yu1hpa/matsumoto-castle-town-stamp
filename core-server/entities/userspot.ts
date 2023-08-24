8f801700e60dbf9938fc7f9186989f1bd874eabb
yu1hpa <yu.1hpa@gmail.com>
2023-08-24 14:56:39 +0900

    feat: Add `User` `Spot` `UserSpot` Entities

---
+++ core-server/entities/userspot.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export default class UserSpot {
+  constructor(userId: string, spotId: string, imgFileName: string) {
+    this.userId = userId;
+    this.spotId = spotId;
+    this.imgFileName = imgFileName;
+  }
+  @PrimaryGeneratedColumn("uuid")
+  public id: string | undefined;
+
+  @Column({ type: "varchar" })
+  public spotId: string;
+
+  @Column({ type: "varchar" })
+  public userId: string;
+
+  @Column({ type: "varchar" })
+  public imgFileName: string;
+
+  @Column({ type: "timestamp" })
+  public visitedAt: Date | undefined;
+
+  @CreateDateColumn({ name: "created_at", type: "timestamp", precision: 0 })
+  readonly createdAt = new Date();
+
+  @UpdateDateColumn({ name: "updated_at", type: "timestamp", precision: 0 })
+  readonly updatedAt = new Date();
+}
+function DateColumn(): (target: UserSpot, propertyKey: "createdAt") => void {
+  throw new Error("Function not implemented.");
+}
