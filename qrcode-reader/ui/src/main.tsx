0dffcd749965eeca2d93159d539958e69383c899
yu1hpa <yu.1hpa@gmail.com>
2023-08-24 10:49:40 +0900

    chore: Delete unnecessary code

--- qrcode-reader/ui/src/main.tsx
+++ qrcode-reader/ui/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 const liffId = import.meta.env.VITE_LIFF_ID
-console.log(`liffId: ${liffId}`);
 liff.init({ liffId }).then(() => {
 ReactDOM.render(
   <React.StrictMode>
